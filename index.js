45c7803d9f42b4c39892916e53ec239850ee62b8
iwatakeshi <iwatakeshi@gmail.com>
2015-03-25 16:55:20 -0600

    Update

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var accept = require('gengojs-accept');
 
-function ship(req) {
-  this.accept = accept(req, {
-    default: this.settings.default(),
-    supported: this.settings.supported(),
-    keys: this.settings.keys(),
-    detect: this.settings.detect()
-  });
+function ship(req, options) {
+  this.accept = accept(req, options);
   return this.accept;
 }
 
-module.exports = function() {
+module.exports = function(options) {
   var register = ship;
   register.package = require('./package');
+  register.package.type = 'accept';
+  register.options = options;
   return register;
 }

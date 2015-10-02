9851bc6e429c2f9d96492886f7cfeeadacfe5725
iwatakeshi <iwatakeshi@gmail.com>
2015-10-02 10:26:02 -0600

    Update
    Updated package.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,19 @@
 /* Dev-Dependencies */
-var
-  gulp        = require('gulp'),
-  sourcemaps  = require('gulp-sourcemaps'),
-  babel       = require('gulp-babel'),
-  mocha       = require('gulp-mocha'),
-  jshint      = require('gulp-jshint'),
-  beautify    = require('gulp-jsbeautify'),
-  shell       = require('gulp-shell'),
-  ghPages     = require('gulp-gh-pages'),
-  rimraf      = require('rimraf'),
-  config      = require('./config'),
-  changelog   = require('gulp-changelog'),
-  semver      = require('semver'),
-  version     = require('node-version').long,
-  isHarmony   = !semver.lt(version.toString(), '0.11.0');
-
-if (!isHarmony) {
-  require("harmonize")(["harmony-generators"]);
-}
+var 
+  gulp = require('gulp'),
+  sourcemaps = require('gulp-sourcemaps'),
+  babel = require('gulp-babel'),
+  mocha = require('gulp-spawn-mocha'),
+  jshint = require('gulp-jshint'),
+  beautify = require('gulp-jsbeautify'),
+  shell = require('gulp-shell'),
+  ghPages = require('gulp-gh-pages'),
+  rimraf = require('rimraf'),
+  config = require('./config'),
+  semver = require('semver'),
+  version = require('node-version').long,
+  isHarmony = !semver.lt(version.toString(), '0.11.0'),
+  changelog = require('gulp-changelog');
 
 /** Backs up the files in case of emergency! */
 gulp.task('backup', function () {
@@ -42,7 +38,7 @@ gulp.task('beautify', ['backup'], function () {
 /*
  * Clean the docs themes folder
  */
-gulp.task('clean:docs', ['gh-pages'], function (cb) {
+gulp.task('clean-docs', ['gh-pages'], function (cb) {
   rimraf('./docs/', cb);
 });
 
@@ -64,7 +60,7 @@ gulp.task('lib', ['beautify'], function () {
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('./source maps/'))
-    .pipe(gulp.dest('./src/'));
+    .pipe(gulp.dest('./'));
 });
 
 /* Watches for changes and applies the build task*/
@@ -74,9 +70,13 @@ gulp.task('watch', function () {
 
 /* Runs tests */
 
-gulp.task('mocha', ['lib'], function () {
-  return gulp.src('./tests/**/*.js')
-    .pipe(mocha());
+gulp.task('test', ['lib'], function (cb) {
+  if (isHarmony)
+    return gulp.src('./tests/**/*.js')
+    .pipe(shell(['mocha --harmony <%= file.path %>']));
+  else return gulp.src([
+    './tests/index.js',
+  ]).pipe(mocha());
 });
 
 gulp.task('changelog', function (cb) {
@@ -90,38 +90,20 @@ gulp.task('changelog', function (cb) {
 * Install other themes here, generate docs for each.
 */
 gulp.task('doc', ['build'], shell.task([
-  (function () {
+  (function(){
     var doc = 'node_modules/mr-doc/bin/mr-doc',
-      cmd = {
-        source: ' -s lib/',
-        output: ' -o docs/',
-        name: ' -n "gengo.js/plugin/header"',
-        theme: ' -t cayman'
-      };
+        cmd = {
+          source: ' -s lib/',
+          output: ' -o docs/',
+          name:' -n "gengo.js/plugin/header"',
+          theme:' -t cayman'
+        };
     return doc + cmd.source + cmd.output + cmd.name + cmd.theme;
   })()
 ]));
 
-/*
- * Clean the backup folder
- */
-gulp.task('clean:backup', function (cb) {
-  rimraf('.backup/', cb);
-});
-
-function end (task){
-  return function(){
-    var isBuild = task ? task.indexOf('build') > -1 : false;
-    var isDocs = task ? task.indexOf('docs') > -1 : false;
-    if(!isBuild && !isDocs)
-      process.exit();
-  };
-}
-
 gulp.task('default', ['backup', 'beautify', 'lib', 'watch']);
 
-gulp.task('build', ['backup', 'beautify', 'lib', 'mocha'], end('build'));
+gulp.task('build', ['backup', 'beautify', 'lib', 'test']);
 
-gulp.task('test', ['mocha'], end());
-
-gulp.task('docs', ['build', 'doc', 'gh-pages', 'clean:docs', 'clean:backup'], end('docs'));
+gulp.task('docs', ['build', 'doc', 'gh-pages', 'clean-docs']);
